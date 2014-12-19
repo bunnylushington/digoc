38a55470dd012e48ab9984c4a64e7dc1e5e90293
Kevin Montuori <montuori@gmail.com>
2014-12-19 15:20:16 -0600

    prelim deps

--- mix.exs
+++ mix.exs
@@ -8,9 +8,6 @@ defmodule DigOc.Mixfile do
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: [:logger]]
   end
@@ -25,6 +22,9 @@ defmodule DigOc.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    []
+    [ 
+     {:poison, "~> 1.3.0"},
+     {:httpoison, "~> 0.5"}
+    ]
   end
 end
