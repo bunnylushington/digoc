fedd17f694d1e17510174c82505142655b79281c
Kevin Montuori <montuori@gmail.com>
2014-12-19 15:34:58 -0600

    token fn

--- lib/digoc.ex
+++ lib/digoc.ex
@@ -1,10 +1,17 @@
 defmodule DigOc do
 
   @endpoint "https://api.digitalocean.com/v2/"
+  @token_varible "DIGOC_API2_TOKEN"
 
   @doc """
   Return the endpoint URL as a string.
   """
   def endpoint, do: @endpoint
+
+  @doc """
+  Return the D.O. API token as a string.
+  """
+  def api_token, do: System.get_env(@token_varible)
+  
 
 end
--- test/digoc_test.exs
+++ test/digoc_test.exs
@@ -5,4 +5,10 @@ defmodule DigOcTest do
     assert is_binary(DigOc.endpoint)
     assert String.starts_with? DigOc.endpoint, "https://"
   end
+
+  test "token" do
+    assert is_binary(DigOc.api_token)
+    assert String.length(DigOc.api_token) == 64
+  end
+
 end
