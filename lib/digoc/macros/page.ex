46de7b9fea109ef2ed76757136bfa9b8e48e45df
Kevin Montuori <montuori@gmail.com>
2015-01-05 13:52:46 -0600

    added brief documentation

--- lib/digoc/macros/page.ex
+++ lib/digoc/macros/page.ex
@@ -4,8 +4,19 @@ defmodule DigOc.Macros.Page do
     predicate = DigOc.predicate(direction)
     bang_name = DigOc.bang(direction)
     quote do
+      @doc """
+      Test if #{ unquote(direction) } page is defined.
+      """
       def unquote(predicate)(data), do: has_page?(data, unquote(direction))
+      
+      @doc """
+      Request #{ unquote(direction) } page.  Returns HTTPoison 3-tuple.
+      """
       def unquote(direction)(data), do: get_page(data, unquote(direction))
+
+      @doc """
+      Request #{ unquote(direction) } page.  Returns only the response body.
+      """
       def unquote(bang_name)(data), do: unquote(direction)(data) |> response
     end
   end
