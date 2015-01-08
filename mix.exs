0e88f270479fdf51218a13d708af4758daa1141a
Kevin Montuori <montuori@gmail.com>
2015-01-07 18:52:59 -0600

    vsn bumped

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule DigOc.Mixfile do
 
   def project do
     [app: :digoc,
-     version: "0.1.0",
+     version: "0.2.0",
      elixir: "~> 1.0",
      description: "An Elixir client for the Digital Ocean API v2.",
      package: package,
